fdf7d9f423cfd1dc7ece0b7eefed06c10fac2fbe
JoaeA <iessochoalmj201617@gmail.com>
2025-03-12 20:33:26 +0100

    2025

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -3,10 +3,11 @@ package db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBConnection {
     // URL de conexión a la base de datos MySQL
-    private static final String URL = "jdbc:mysql://192.168.18.50:3366/mi_DB";
+    private static final String URL = "jdbc:mysql://localhost:3306/midb";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root123456";
 
@@ -24,6 +25,7 @@ public class DBConnection {
                     try {
                        // Establecer la conexión
                         connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                        crearTabla(connection);
                     } catch ( SQLException e) {
                         e.printStackTrace();
                     }
@@ -31,6 +33,19 @@ public class DBConnection {
             }
         }
         return connection;
+    }
+    private static void crearTabla(Connection conexion) throws SQLException {
+        try (Statement statement = conexion.createStatement()) {
+            statement.executeUpdate(
+                    "CREATE TABLE IF NOT EXISTS Persona (\n" +
+                            "    dni VARCHAR(10) PRIMARY KEY,\n" +
+                            "    nombre VARCHAR(50) NOT NULL,\n" +
+                            "    apellido VARCHAR(50) NOT NULL,\n" +
+                            "    edad INT CHECK (edad >= 0)\n" +
+                            ");"
+            );
+            System.out.println("Tabla Alumno creada correctamente.");
+        }
     }
 
     // Método para cerrar la conexión
